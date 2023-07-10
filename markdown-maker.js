a0d7f612d1bf9ef16f30b7497a3ab62e4fadc46b
ArcCosine <40669+ArcCosine@users.noreply.github.com>
2023-07-11 07:42:14 +0900

    [Update] Update markdown maker.

--- markdown-maker.js
+++ markdown-maker.js
@@ -1,32 +1,38 @@
-(function(){
-    "use strict";
+const marked = require("marked");
+const fs = require("fs");
+const args = process.argv.slice(2);
 
-    const fs = require("fs");
-    const args = process.argv.slice(2);
+if (!args[0]) {
+    console.log("Usage:");
+    console.log("node " + process.argv[1] + " [file]");
+    return -1;
+}
 
-    if( !args[0] ){
-        console.log("Usage:");
-        console.log("node " + process.argv[1] + " [file]" );
-        return -1;
-    }
+const renderer = new marked.Renderer();
+renderer.heading = function (text, level ) {
+    return "<h" + level + ">" + text + "</h" + level + ">";
+};
 
-    const marked = require("marked");
-    const renderer = new marked.Renderer();
-    renderer.heading = function( text, level ){
-        return "<h" + level + ">" + text + "</h" + level + ">";
-    }
-    marked.setOptions({
-        renderer: renderer,
-        gfm: true,
-        tables: true,
-        breaks: false,
-        pedantic: false,
-        sanitize: false,
-        smartLists: true,
-        smartypants: false
-    });
+renderer.link = function (href, title, text) {
+    const external = !/^\//.test(href);
+    const target = external ? ' target="_blank"' : "";
+    const titleText = title ? ' title="' + title + '"' : "";
+    const hrefText = ' href="' + href + '"';
 
-    const text = fs.readFileSync(args[0],"utf-8")
-    const html = marked(text);
-    console.log(html);
-})();
+    return "<a" + hrefText + target + titleText + ">" + text + "</a>";
+};
+
+marked.setOptions({
+    renderer: renderer,
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false,
+});
+
+const text = fs.readFileSync(args[0], "utf-8");
+const html = marked(text);
+console.log(html);
